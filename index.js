6232fd27e996bb7ff54b6681fc42d99f7f1cd921
Dylan Smith <dylan@rh.izo.me>
2015-10-08 17:47:21 -0700

    wip

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 'use strict'
 
-module.exports = require('./lib/manager')
+module.exports = require('./lib/pinboard_manager')
--- test/test.js
+++ test/test.js
@@ -1,4 +1,10 @@
 var expect = require("chai").expect
-var app = require("../lib/manager.js")
+var app = require("../lib/pinboard_manager.js")
 
-
+describe("PinboardManager", function() {
+  describe("#setupAPIToken", function() {
+    it("exits if no API key found", function() {
+      expect(PinboardManager.api_key).to.equal("test-api-key")
+    })
+  })
+})
